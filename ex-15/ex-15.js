de5f53fcb27c30648feee9795d6d50e2393fccaa
GabrielJFS <166074513+gabrieljaivona@users.noreply.github.com>
2024-05-21 08:17:52 -0400

    Adicionando o CSS e o JS

---
+++ ex-15/ex-15.js
@@ -0,0 +1,6 @@
+function contar(){
+    let inicio = document.getElementById('inumero-inicio')
+    let fim = document.getElementById('inumero-fim')
+    let numeroPasso = getElementById('inumero-passo')
+    //while (num1)
+}
