78f34974372b5fc692458b824b80df6162ef2288
GabrielJFS <166074513+gabrieljaivona@users.noreply.github.com>
2024-05-22 09:40:38 -0400

    Consertando pequeno erro

--- ex-15/ex-15.js
+++ ex-15/ex-15.js
@@ -11,7 +11,9 @@ function contar(){
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+        }
         if (i < f) {
             //contagem crescente
             for(let c = i; c <= f; c += p){
